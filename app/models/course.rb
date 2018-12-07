97e6b988bbe96d46cab733d5ed08db5e140e84fa
Franco Bulgarelli <franco@mumuki.org>
2018-12-07 14:30:42 -0300

    Moving code from classroom-api to here

--- app/models/course.rb
+++ app/models/course.rb
@@ -6,6 +6,8 @@ class Course < ApplicationRecord
   validates_uniqueness_of :slug
   belongs_to :organization
 
+  has_one :invitation
+
   def import_from_resource_h!(resource_h)
     update! Mumukit::Platform::Course::Helpers.slice_platform_json(resource_h)
   end
@@ -17,6 +19,21 @@ class Course < ApplicationRecord
     self[:code] = s.course
     self[:organization_id] = Organization.find_by!(name: s.organization).id
   end
+
+  def invite!(expiration_date)
+    generate_invitation! expiration_date if closed?
+    invitation
+  end
+
+  def closed?
+    invitation.blank? || invitation.expired?
+  end
+
+  def generate_invitation!(expiration_date)
+    invitation = build_invitation expiration_date: expiration_date, course: self
+    invitation.save_and_notify!
+  end
+
 
   def self.sync_key_id_field
     :slug
--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,23 +1,32 @@
+# invitation:
+#   course
+#   code
+#   expiration_date
+
 class Invitation < ApplicationRecord
-  def self.find_by_code(code)
-    Invitation
-      .where(code: code)
-      .where(Invitation.arel_table[:expiration_date].gt(Time.now))
-      .take!
-  rescue RangeError
-    raise RecordNotFound.new('The invitation does not exist or it has expired')
+  include Syncable
+
+  belongs_to :course
+  validates_uniqueness_of :code
+
+  defaults do
+    self.code = self.class.generate_code
   end
 
-  def self.import_from_resource_h!(json)
-    Invitation.create! json
+  def import_from_resource_h!(json)
+    update! json
   end
 
   def organization
-    Organization.find_by! name: course.to_mumukit_slug.organization
+    course.organization
   end
 
   def navigable_name
     I18n.t(:invitation_for, course: course_name)
+  end
+
+  def to_resource_h(*)
+    { code: code, course: course.slug, expiration_date: expiration_date }
   end
 
   def navigation_end?
@@ -28,9 +37,30 @@ class Invitation < ApplicationRecord
     code
   end
 
+  def expired?
+    Time.now > expiration_date
+  end
+
+  def unexpired
+    raise RecordNotFound, "This invitation has already expired" if expired?
+    self
+  end
+
+  def self.generate_code
+    SecureRandom.urlsafe_base64 4
+  end
+
+  def notify!
+    Mumukit::Nuntius.notify_event! 'InvitationChanged', invitation: to_resource_h
+  end
+
   private
 
   def course_name
-    course.to_mumukit_slug.course
+    course.name
+  end
+
+  def self.sync_key_id_field
+    :code
   end
 end
