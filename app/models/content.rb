7614a9979551fbf98f5d4609e6397aa34362d6e3
Franco Bulgarelli <franco@mumuki.org>
2019-02-11 20:55:32 -0300

    Failing before creating children

--- app/models/content.rb
+++ app/models/content.rb
@@ -16,8 +16,10 @@ class Content < ApplicationRecord
     rebased_dup(organization).tap do |dup|
       self.class.find_by(slug: dup.slug).try { |it| return it } if quiet
 
+      dup.validate!
       fork_children_into! dup, organization, syncer
-      dup.save!
+      dup.save validate: false
+
       syncer.export! dup
     end
   end
