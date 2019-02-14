f77ed21b8382b73adf34ff42a85da1124ed674a6
Rodrigo Alfonso <rodri042@gmail.com>
2019-02-14 15:34:34 -0300

    Normalizing slugs before create

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -8,6 +8,8 @@ class Guide < Content
   numbered :exercises
   has_many :exercises, -> { order(number: :asc) }, dependent:  :delete_all
 
+  before_create :normalize_slug!
+
   self.inheritance_column = nil
 
   enum type: [:learning, :practice]
@@ -55,6 +57,10 @@ class Guide < Content
 
   def done_for?(user)
     stats_for(user).done?
+  end
+
+  def normalize_slug!
+    self.slug = self.slug.to_mumukit_slug.normalize.to_s
   end
 
   def import_from_resource_h!(resource_h)
