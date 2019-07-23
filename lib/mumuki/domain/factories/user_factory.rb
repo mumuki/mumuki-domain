5f60a40e4008eab159e8e96ad07828c23bd54772
Julián Berbel Alt <julianberbel@hotmail.com>
2019-07-23 17:29:54 -0300

    Add gender and birthdate to user factory

--- lib/mumuki/domain/factories/user_factory.rb
+++ lib/mumuki/domain/factories/user_factory.rb
@@ -4,5 +4,7 @@ FactoryBot.define do
     uid { email }
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
+    gender { 1 }
+    birthdate { Date.today }
   end
 end
