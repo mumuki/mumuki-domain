9a40d0838c1e79b1c4be01347e26e40a1b517315
Lucho Cannavo <luchotc@hotmail.com.ar>
2019-12-04 16:48:43 -0300

    Making migration fully reversible

--- db/migrate/20191022180238_remove_choice_values_from_exercises.rb
+++ db/migrate/20191022180238_remove_choice_values_from_exercises.rb
@@ -1,5 +1,5 @@
 class RemoveChoiceValuesFromExercises < ActiveRecord::Migration[5.1]
   def change
-    remove_column :exercises, :choice_values, :string
+    remove_column :exercises, :choice_values, :string, array: true, default: [], null: false
   end
 end
