21b4e6d997e130d7993bfd0bf8b19eea8537cd37
Julián Berbel Alt <julianberbel@hotmail.com>
2019-12-05 15:47:22 -0300

    Fix children association

--- app/models/indicator.rb
+++ app/models/indicator.rb
@@ -3,7 +3,8 @@ class Indicator < ApplicationRecord
   belongs_to :content, polymorphic: true
   belongs_to :organization
 
-  has_many :children, as: :parent
+  has_many :children, foreign_key: :parent_id, class_name: 'Indicator'
+  has_many :assignments, foreign_key: :parent_id
 
   include Progress
 
