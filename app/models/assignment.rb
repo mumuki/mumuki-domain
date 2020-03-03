bc9809fb23c10b5ab7f8cca047244b17d045ca0c
Lucho Cannavo <luchotc@hotmail.com.ar>
2020-03-03 17:37:54 -0300

    Persisting organization in save_submission! instead of save!

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -205,11 +205,6 @@ class Assignment < Progress
     exercise.files_for(current_content)
   end
 
-  def save!(*)
-    self.organization = Organization.current
-    super
-  end
-
   private
 
   def update_submissions_count!
