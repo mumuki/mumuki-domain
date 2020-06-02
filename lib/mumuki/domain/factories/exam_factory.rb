cc5bb8d49b98e8a7935a344d15bfb5c60d08facb
Felipe Calvo <felipe@mumuki.org>
2020-06-02 13:05:30 -0300

    Add named parameters on factories using Faker

--- lib/mumuki/domain/factories/exam_factory.rb
+++ lib/mumuki/domain/factories/exam_factory.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
 
   factory :exam, traits: [:guide_container] do
-    duration { Faker::Number.between(10, 60).minutes }
+    duration { Faker::Number.between(from: 10, to:60).minutes }
     organization { Organization.current }
     start_time { 5.minutes.ago }
     end_time { 10.minutes.since }
