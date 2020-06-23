c89fb6b6e8c98e4daa6d616b512500afa5922c9b
Felipe Calvo <felipe@mumuki.org>
2020-06-23 16:06:38 -0300

    Add top_submission_status to Assignments

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200616160640) do
+ActiveRecord::Schema.define(version: 20200617142217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20200616160640) do
     t.bigint "organization_id"
     t.datetime "submitted_at"
     t.bigint "parent_id"
+    t.integer "top_submission_status", default: 0
     t.index ["exercise_id"], name: "index_assignments_on_exercise_id"
     t.index ["organization_id"], name: "index_assignments_on_organization_id"
     t.index ["parent_id"], name: "index_assignments_on_parent_id"
