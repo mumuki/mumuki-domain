48878401b621d1e219e802ebcafcd91e00a6dc6f
Federico Scarpa <fedescarpa@gmail.com>
2020-06-24 15:24:15 -0300

    fixing factories

--- lib/mumuki/domain/factories/exam_factory.rb
+++ lib/mumuki/domain/factories/exam_factory.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
   factory :exam, traits: [:guide_container] do
     duration { Faker::Number.between(from: 10, to:60).minutes }
     organization { Organization.current }
-    course { create(:course, slug: 'test/foo') }
+    course { create(:course) }
     start_time { 5.minutes.ago }
     end_time { 10.minutes.since }
     passing_criterion_type { 'none' }
