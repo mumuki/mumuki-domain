a55a7e229d01292b7a5573589745c81ba84aaab5
Franco Bulgarelli <franco@mumuki.org>
2020-07-24 15:26:13 -0300

    Delegating solution_for

--- app/models/concerns/submittable/solvable.rb
+++ app/models/concerns/submittable/solvable.rb
@@ -1,12 +1,16 @@
 module Solvable
-  def submit_solution!(user, attributes={})
-    assignment, _ = find_assignment_and_submit! user, attributes[:content].to_mumuki_solution(language)
+  def submit_solution!(user, submission_attributes={})
+    assignment, _ = find_assignment_and_submit! user, solution_for(submission_attributes)
     try_solve_discussions(user) if assignment.solved?
     assignment
   end
 
   def run_tests!(params)
     language.run_tests!(params.merge(locale: locale, expectations: expectations, custom_expectations: custom_expectations))
+  end
+
+  def solution_for(submission_attributes)
+    submission_attributes[:content].to_mumuki_solution(language)
   end
 end
 
