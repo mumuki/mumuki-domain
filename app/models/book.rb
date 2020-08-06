cff036f4df183015303ef0031093fda283402e89
Lucho Cannavo <luchotc@hotmail.com.ar>
2020-08-06 18:21:06 -0300

    Removing unnecessary all

--- app/models/book.rb
+++ app/models/book.rb
@@ -18,7 +18,7 @@ class Book < Content
   end
 
   def discussions_in_organization(organization = Organization.current)
-    Discussion.all.where(organization: organization).includes(exercise: [:language, :guide])
+    Discussion.where(organization: organization).includes(exercise: [:language, :guide])
   end
 
   def first_chapter
