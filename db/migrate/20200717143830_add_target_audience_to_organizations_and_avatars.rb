bae6f545f4348703e5dc24f8acaee847338dadef
Lucho Cannavo <luchotc@hotmail.com.ar>
2020-09-08 17:32:24 -0300

    Renaming migration name

--- db/migrate/20200717143830_add_target_audience_to_organizations_and_avatars.rb
+++ db/migrate/20200717143830_add_target_audience_to_organizations_and_avatars.rb
@@ -1,4 +1,4 @@
-class AddTargetVisualIdentityToOrganizationsAndAvatars < ActiveRecord::Migration[5.1]
+class AddTargetAudienceToOrganizationsAndAvatars < ActiveRecord::Migration[5.1]
   def change
     add_column :organizations, :target_audience, :integer, default: 0
     add_column :avatars, :target_audience, :integer, default: 0
