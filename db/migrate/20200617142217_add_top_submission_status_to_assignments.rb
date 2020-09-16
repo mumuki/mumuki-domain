cae1d3defaa238d22c8c97eea5d77814092b96fd
Lucho Cannavo <luchotc@hotmail.com.ar>
2020-09-16 12:03:14 -0300

    Removing database default for top_submission_status

--- db/migrate/20200617142217_add_top_submission_status_to_assignments.rb
+++ db/migrate/20200617142217_add_top_submission_status_to_assignments.rb
@@ -1,5 +1,5 @@
 class AddTopSubmissionStatusToAssignments < ActiveRecord::Migration[5.1]
   def change
-    add_column :assignments, :top_submission_status, :integer, default: 0
+    add_column :assignments, :top_submission_status, :integer
   end
 end
