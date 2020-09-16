4250e36b09149a960b4f4493d0af3a689c32b5e4
Felipe Calvo <felipe@mumuki.org>
2020-09-16 15:11:07 -0300

    Refactor Awardee acquired and unacquired medals

--- app/models/concerns/awardee.rb
+++ app/models/concerns/awardee.rb
@@ -1,9 +1,23 @@
 module Awardee
   def acquired_medals
-    Indicator.where(organization: Organization.current, user: self).select(&:completed?).map { |i| i.content.medal }.compact
+    medals_for all_medals.first
   end
 
   def unacquired_medals
-    Indicator.where(organization: Organization.current, user: self).select {|i| !i.completed? }.map { |i| i.content.medal }.compact
+    medals_for all_medals.second
+  end
+
+  private
+
+  def medals_for(indicators)
+    indicators.map { |i| i.content.medal }.compact
+  end
+
+  def all_medals
+    current_indicators.partition(&:completed?)
+  end
+
+  def current_indicators
+    Indicator.where(organization: Organization.current, user: self)
   end
 end
