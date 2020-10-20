291038e8d70a04940e4d533810e53ebbbb30cd64
Felipe Calvo <felipe@mumuki.org>
2020-10-20 09:56:34 -0300

    Optimize medal querying

--- app/models/concerns/awardee.rb
+++ app/models/concerns/awardee.rb
@@ -1,10 +1,10 @@
 module Awardee
   def acquired_medals
-    medals_for awardable_content_progress_here.select(&:once_completed?)
+    @acquired_medals ||= medals_for awardable_content_progress_here.select(&:once_completed?)
   end
 
   def unacquired_medals
-    medals_for awardable_content_progress_here.reject(&:once_completed?)
+    @unacquired_medals ||= medals_for awardable_content_progress_here.reject(&:once_completed?)
   end
 
   private
@@ -14,7 +14,7 @@ module Awardee
   end
 
   def awardable_content_progress_here
-    awardable_contents_here.map { |i| i.progress_for(self, Organization.current) }
+    @awardable_content_progress_here ||= awardable_contents_here.map { |i| i.progress_for(self, Organization.current) }
   end
 
   def awardable_contents_here
