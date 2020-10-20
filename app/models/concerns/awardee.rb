018a25674d184ac96316a8d56ae08f1c45726e70
Felipe Calvo <felipe@mumuki.org>
2020-10-20 09:31:50 -0300

    Rename awardee methods

--- app/models/concerns/awardee.rb
+++ app/models/concerns/awardee.rb
@@ -1,23 +1,23 @@
 module Awardee
   def acquired_medals
-    medals_for current_indicators.select(&:once_completed?)
+    medals_for awardable_content_progress_here.select(&:once_completed?)
   end
 
   def unacquired_medals
-    medals_for current_indicators.reject(&:once_completed?)
+    medals_for awardable_content_progress_here.reject(&:once_completed?)
   end
 
   private
 
-  def medals_for(indicators)
-    indicators.map { |i| i.content.medal }.compact
+  def medals_for(progress)
+    progress.map { |i| i.content.medal }.compact
   end
 
-  def current_indicators
-    items_with_medals.map { |i| i.progress_for(self, Organization.current) }
+  def awardable_content_progress_here
+    awardable_contents_here.map { |i| i.progress_for(self, Organization.current) }
   end
 
-  def items_with_medals
+  def awardable_contents_here
     Usage.where(organization: Organization.current).map(&:item).select(&:medal_id)
   end
 end
