35e21e717395b7ab8414a555b41d2057fc8f9487
Julián Berbel Alt <julian@mumuki.org>
2020-11-13 15:41:53 -0300

    Add tests for desired behavior

--- spec/models/solution_spec.rb
+++ spec/models/solution_spec.rb
@@ -139,4 +139,26 @@ describe Mumuki::Domain::Submission::Solution, organization_workspace: :test do
       it { expect(assignment.expectation_results).to eq([binding: '<<custom>>', inspection: 'foo uses composition', result: :passed]) }
     end
   end
+
+  describe '#submit_solution!' do
+    let(:organization)  { create :organization }
+    let(:organization2) { create :organization, name: 'orga2', book: organization.book }
+    let(:user) { create :user }
+
+    before { organization.switch! }
+    let(:exercise) { create :indexed_exercise }
+    let(:guide) { exercise.guide }
+    let!(:assignment) { exercise.submit_solution!(user, content: 'foo') }
+
+    before do
+      organization2.switch!
+      exercise.submit_solution!(user, content: 'bar')
+      assignment.reload
+    end
+
+    it { expect(assignment.solution).to eq('bar') }
+    it { expect(assignment.parent).to_not eq(guide.progress_for(user, organization )) }
+    it { expect(assignment.parent).to     eq(guide.progress_for(user, organization2)) }
+    it { expect(guide.progress_for(user, organization)).to be_dirty_by_submission }
+  end
 end
