cb00ccea5f683ac9b51d0a3a6e79e951fb77d2fd
Lucho Cannavo <luchotc@hotmail.com.ar>
2020-11-19 16:43:19 -0300

    Reduce terms default size in factory

--- lib/mumuki/domain/factories/term_factory.rb
+++ lib/mumuki/domain/factories/term_factory.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :term do
-    content { Faker::Lorem.sentence(word_count: 2000) }
+    content { Faker::Lorem.sentence(word_count: 100) }
     header { Faker::Lorem.sentence(word_count: 5) }
   end
 end
