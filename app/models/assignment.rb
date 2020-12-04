ab5273facbf2455216d97880e663fa0066fd879f
Lucho Cannavo <luchotc@hotmail.com.ar>
2020-12-04 11:37:43 -0300

    Revert content setter method change

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -126,7 +126,7 @@ class Assignment < Progress
   end
 
   def content=(content)
-    unless exercise.reading?
+    if content.present?
       self.solution = exercise.single_choice? ? exercise.choice_index_for(content) : content
     end
   end
