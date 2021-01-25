fb97336b8f8adef882d1cc729a167e7c095bb822
Franco Bulgarelli <franco@mumuki.org>
2021-01-25 03:54:30 -0300

    7th optimization: avoid usages for pending lessons

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -50,18 +50,19 @@ class Topic < Content
   end
 
   def pending_lessons(user)
-    guides.
-      joins('left join public.exercises exercises
-                on exercises.guide_id = guides.id').
-      joins("left join public.assignments assignments
+    lessons
+      .includes(:guide)
+      .references(:guide)
+      .joins('left join exercises exercises on exercises.guide_id = guides.id')
+      .joins("left join assignments assignments
                 on assignments.exercise_id = exercises.id
                 and assignments.submitter_id = #{user.id}
                 and assignments.submission_status in (
                   #{Mumuki::Domain::Status::Submission::Passed.to_i},
                   #{Mumuki::Domain::Status::Submission::ManualEvaluationPending.to_i}
-                )").
-      where('assignments.id is null').
-      group('public.guides.id', 'lessons.number').map(&:lesson)
+                )")
+      .where('assignments.id is null')
+      .group('guides.id', 'lessons.number', 'lessons.id')
   end
 
   private
