c2b6addfb771d939daf6704ee17dbba4047c190d
Federico Aloi <federico.aloi@gmail.com>
2021-01-26 18:15:01 -0300

    Method to mark notification as read

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,4 +2,8 @@ class Notification < ApplicationRecord
   belongs_to :user
   belongs_to :organization
   belongs_to :target, polymorphic: true
+
+  def self.mark_as_read!(target)
+    find_by!(target: target).update(read: true)
+  end
 end
