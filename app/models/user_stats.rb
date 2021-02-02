945af8dbbef306cf5e27e523d9b8fc4e6123ee4e
Federico Aloi <federico.aloi@gmail.com>
2021-02-02 16:40:29 -0300

    Add activity stats to UserStats

--- app/models/user_stats.rb
+++ app/models/user_stats.rb
@@ -10,7 +10,39 @@ class UserStats < ApplicationRecord
     self.stats_for(user).exp
   end
 
+  def activity(date_range = nil)
+    {
+        exercises: {
+            solved_count: organization_exercises
+                              .joins(:assignments)
+                              .where(assignments: { top_submission_status: [:passed, :skipped], submitter: user }.merge_if_present(:submitted_at, date_range))
+                              .count,
+            count: organization_exercises.count},
+
+        messages: {
+            count: messages_in_discussions(date_range).count,
+            approved: messages_in_discussions(date_range).where(approved: true).count}
+    }
+  end
+
   def add_exp!(points)
     self.exp += points
+  end
+
+  private
+
+  def messages_in_discussions(date_range = nil)
+    Message.joins(:discussion)
+        .where({sender: user.uid, discussions: { organization: organization }}.merge_if_present(:date, date_range))
+  end
+
+  def organization_exercises
+    organization.book.exercises
+  end
+end
+
+class Hash
+  def merge_if_present(key, value)
+    value ? merge({ key => value }) : self
   end
 end
