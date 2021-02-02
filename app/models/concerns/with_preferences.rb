461eeda8da4dfe8db9c817bf477808707ad96013
Felipe Calvo <felipecalvo239@gmail.com>
2021-02-02 16:35:39 -0300

    Add uppercase? alias for uppercase_mode

--- app/models/concerns/with_preferences.rb
+++ app/models/concerns/with_preferences.rb
@@ -4,4 +4,8 @@ module WithPreferences
   included do
     composed_of :preferences, mapping: %w(uppercase_mode uppercase_mode), constructor: :from_attributes
   end
+
+  def uppercase?
+    uppercase_mode
+  end
 end
