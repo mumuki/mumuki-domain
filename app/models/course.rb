1813341462cfa554b356d2dad28ea85a9f02efb5
Franco Leonardo Bulgarelli <franco@mumuki.org>
2021-02-12 11:04:12 -0300

    Remove shifts and days presence validation

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,7 +3,7 @@ class Course < ApplicationRecord
   include Mumuki::Domain::Helpers::Course
   include Mumuki::Domain::Area
 
-  validates_presence_of :slug, :shifts, :code, :days, :period, :description, :organization_id
+  validates_presence_of :slug, :period, :code, :description, :organization_id
   validates_uniqueness_of :slug
   belongs_to :organization
 
