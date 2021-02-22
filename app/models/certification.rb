04c514cce3fd948a4d23a6cc91cba219c89b3837
Federico Scarpa <fedescarpa@gmail.com>
2021-02-22 09:49:27 -0300

    Added default template

--- app/models/certification.rb
+++ app/models/certification.rb
@@ -5,4 +5,38 @@ class Certification < ApplicationRecord
   def friendly
     title
   end
+
+  def template_html_erb
+    self[:template_html_erb] ||= <<HTML
+<style>
+  .qr-code {
+    bottom: 5px;
+    right: 5px;
+    height: 15mm;
+    width: 15mm;
+  }
+  .name {
+    position: absolute;
+    width: 100%;
+    top: 380px;
+    text-align: center;
+  }
+</style>
+<!-- You can use interpolations like --
+  <%#= start_date %>
+  <%#= end_date %>
+  <%#= user.formal_first_name %>
+  <%#= user.formal_last_name %>
+  <%#= user.formal_full_name %>
+  <%#= certification.title %>
+  <%#= certification.description %>
+  <%#= organization.name %>
+  <%#= organization.display_name %>
+--                                  -->
+<section class="name">
+    <h1><%= user.formal_full_name %></h1>
+</section>
+HTML
+  end
+
 end
