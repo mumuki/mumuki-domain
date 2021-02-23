0a571cad470b9dd0b5aeefe5997c8de342c5b3b2
Julián Berbel Alt <julian@mumuki.org>
2021-02-23 15:34:30 -0300

    Remove fixed TODO

--- app/models/concerns/with_assignments.rb
+++ app/models/concerns/with_assignments.rb
@@ -19,7 +19,6 @@ module WithAssignments
   end
 
   # TODO: When the organization is used in this one, please change guide.pending_exercises
-  # TODO: Please do the same on WithAssignmentsBatch
   def find_assignment_for(user, organization)
     assignments.find_by(submitter: user, organization: organization)
   end
