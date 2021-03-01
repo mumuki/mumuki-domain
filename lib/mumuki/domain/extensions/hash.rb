e9cd9896b985e4a62c812487f6b5547d132297b4
Lucho Cannavo <luchotc@hotmail.com.ar>
2021-03-01 16:22:26 -0300

    Remove to_deep_struct methods

--- lib/mumuki/domain/extensions/hash.rb
+++ lib/mumuki/domain/extensions/hash.rb
@@ -15,11 +15,4 @@ class Hash
   def randomize_with(randomizer, seed)
     transform_values { |v| v.randomize_with randomizer, seed }
   end
-
-  def to_deep_struct
-    hash_os = each_with_object({}) do |(key, val), memo|
-      memo[key] = (val.is_a?(Hash) || val.is_a?(Array)) ? val.to_deep_struct : val
-    end
-    OpenStruct.new(hash_os)
-  end
 end
