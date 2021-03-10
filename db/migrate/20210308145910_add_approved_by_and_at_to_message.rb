31a0ab2c40f9cb8e11911b0ae1fba208422d7625
Federico Scarpa <fedescarpa@gmail.com>
2021-03-10 14:55:22 -0300

    fixing typo

--- db/migrate/20210308145910_add_approved_by_and_at_to_message.rb
+++ db/migrate/20210308145910_add_approved_by_and_at_to_message.rb
@@ -1,4 +1,4 @@
-class AddApprobedByAndAtToMessage < ActiveRecord::Migration[5.1]
+class AddApprovedByAndAtToMessage < ActiveRecord::Migration[5.1]
   def change
     add_column :messages, :approved_at, :datetime
     add_reference :messages, :approved_by, index: true
