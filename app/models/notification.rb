939e5b841adcefb266df19de089a68097c1c8a9a
Lucho Cannavo <luchotc@hotmail.com.ar>
2021-03-30 17:50:30 -0300

    Introduce new register! method

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,6 +3,11 @@ class Notification < ApplicationRecord
   belongs_to :organization
   belongs_to :target, polymorphic: true
 
+
+  scope :notified_users_ids_for, ->(target, organization=Organization.current) do
+    where(target: target, organization: organization).pluck(:user_id)
+  end
+
   def mark_as_read!
     update read: true
   end
