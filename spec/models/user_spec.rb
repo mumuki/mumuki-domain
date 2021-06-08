4ba3c4ceba7feb66979419429ef7df1c817e5d71
Federico Scarpa <fedescarpa@gmail.com>
2021-06-08 13:37:30 -0300

    Add ex_student logic

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -787,4 +787,19 @@ describe User, organization_workspace: :test do
       it { expect(messages_count).to eq 1 }
     end
   end
+
+  describe '#solved_any_exercises?' do
+    let(:user) { build :user, permissions: { student: 'test/all' }}
+    let(:ex_orga) { create :organization, name: 'ex' }
+    let(:test_orga) { Organization.locate!('test') }
+
+    before do
+      ex_orga.switch!
+      build(:indexed_exercise).submit_solution!(user, content: '').passed!
+    end
+
+    it { expect(user.solved_any_exercises?(test_orga)).to eql false }
+    it { expect(user.solved_any_exercises?(ex_orga)).to eql true }
+    it { expect(user.solved_any_exercises?).to eql true }
+  end
 end
