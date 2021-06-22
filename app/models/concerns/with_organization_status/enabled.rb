9db0e763e428e4bb71d59d3a99755bef732fed02
Federico Scarpa <fedescarpa@gmail.com>
2021-06-22 15:27:27 -0300

    Add teacher access mode

--- app/models/concerns/with_organization_status/enabled.rb
+++ app/models/concerns/with_organization_status/enabled.rb
@@ -1,5 +1,9 @@
 class WithOrganizationStatus::Enabled < WithOrganizationStatus::Base
 
+  def teacher_access_mode(user)
+    OrganizationAccessMode::Full.new(user)
+  end
+
   def student_access_mode(user)
     OrganizationAccessMode::Full.new user
   end
