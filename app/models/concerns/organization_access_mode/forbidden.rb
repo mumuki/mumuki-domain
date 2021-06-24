8207e0a2ffd8ba26e08a39cd77f594e61736152b
Federico Scarpa <fedescarpa@gmail.com>
2021-06-24 15:55:34 -0300

    Inherit Forbidden from ComingSoon access_mode

--- app/models/concerns/organization_access_mode/forbidden.rb
+++ app/models/concerns/organization_access_mode/forbidden.rb
@@ -1,13 +1,5 @@
-class OrganizationAccessMode::Forbidden < OrganizationAccessMode::Base
+class OrganizationAccessMode::Forbidden < OrganizationAccessMode::ComingSoon
   def validate_active!
-    raise Mumuki::Domain::ForbiddenError unless Organization.current.public? || !user
-  end
-
-  def faqs_here?
-    false
-  end
-
-  def profile_here?
-    false
+    raise Mumuki::Domain::ForbiddenError unless organization.public? || !user
   end
 end
