74a7bf7b81f2a469731f7d9e9c972bebfb7cb894
Federico Scarpa <fedescarpa@gmail.com>
2021-08-13 11:27:55 -0300

    Extract if to private method

--- app/models/concerns/with_organization_status.rb
+++ app/models/concerns/with_organization_status.rb
@@ -1,13 +1,7 @@
 module WithOrganizationStatus
 
   def status
-    if disabled?
-      @status ||= WithOrganizationStatus::Disabled.new self
-    elsif in_preparation?
-      @status ||= WithOrganizationStatus::InPreparation.new self
-    else
-      @status ||= WithOrganizationStatus::Enabled.new self
-    end
+    @status ||= _status
   end
 
   def access_mode(user)
@@ -21,6 +15,18 @@ module WithOrganizationStatus
   def validate_active_for!(user)
     status.validate!(user)
     access_mode(user).validate_active!
+  end
+
+  private
+
+  def _status
+    if disabled?
+      WithOrganizationStatus::Disabled.new self
+    elsif in_preparation?
+      @status ||= WithOrganizationStatus::InPreparation.new self
+    else
+      @status ||= WithOrganizationStatus::Enabled.new self
+    end
   end
 
 end
