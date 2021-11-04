b1f5930b0daf30b41dee3d6e0a3acc14dbfac0f5
Julián Berbel Alt <julian@mumuki.org>
2021-11-04 18:56:01 -0300

    Readd by_status scope as it was being used after all

--- app/models/concerns/with_discussion_status.rb
+++ app/models/concerns/with_discussion_status.rb
@@ -4,6 +4,7 @@ module WithDiscussionStatus
   included do
     serialize :status, Mumuki::Domain::Status::Discussion
     validates_presence_of :status
+    scope :by_status, -> (status) { where(status: status) }
   end
 
   delegate :closed?, :opened?, :solved?, :pending_review?, :reachable_statuses, to: :status
